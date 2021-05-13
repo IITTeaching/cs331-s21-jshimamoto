4355083bb7ca255c73ae922ba51ee39784c698b9
Joseph Shimamoto <jshimamoto@hawk.iit.edu>
2021-05-12 21:39:04 -0500

    Finished Radix Sort

--- project/radix-sort.py
+++ project/radix-sort.py
@@ -1,5 +1,5 @@
 import urllib
-import requests
+import urllib.request
 
 def book_to_words(book_url='https://www.gutenberg.org/files/84/84-0.txt'):
     booktxt = urllib.request.urlopen(book_url).read().decode()
@@ -7,4 +7,30 @@ def book_to_words(book_url='https://www.gutenberg.org/files/84/84-0.txt'):
     return bookascii.split()
 
 def radix_a_book(book_url='https://www.gutenberg.org/files/84/84-0.txt'):
-    pass
+    lst = book_to_words()
+    longest = len(max(lst,key = len)) - 2
+    sorted = countSort(lst, longest + 1)
+    for i in range(longest, -1, -1):
+        sorted = countSort(sorted, i)
+    return sorted
+
+def countSort(lst, num):
+    counter = [0] * 128
+    shi = [None] * len(lst)
+    for i in lst:
+        if len(i) - 1 < num:
+            k = 0
+        else:
+            k = ord(i.decode('ascii')[num])
+        counter[k] += 1
+    for i in range(1, len(counter)):
+        counter[i] += counter[i - 1]
+    for i in range(len(lst) - 1, -1, -1):
+        if len(lst[i]) - 1 < num:
+            k = 0
+        else:
+            k = ord(lst[i].decode('ascii')[num])
+        shi[counter[k] - 1] = lst[i]
+        counter[k] += -1
+    return shi
+print(radix_a_book())
